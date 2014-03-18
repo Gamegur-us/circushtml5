0b1af620ae965f613a58072df975ffd112160879
Eugenio casa <eugenioclrc@gmail.com>
2014-03-18 01:40:43 -0300

    move some loads to the preloader, and load more images for stage02

--- src/js/states/Preloader.js
+++ src/js/states/Preloader.js
@@ -36,7 +36,8 @@ GameCtrl.Preloader.prototype = {
 		this.load.setPreloadSprite(this.preloadBar);
 
 		//	Here we load the rest of the assets our game needs.		
-		this.load.image('background', 'assets/images/background.png');
+		this.load.image('stage01', 'assets/images/stage01.png');
+		this.load.image('stage02', 'assets/images/stage02.png');
 		
 		//  This is how you load an atlas
 		//this.load.atlas('playButton', 'assets/images/play_button.png', 'assets/images/play_button.json');
@@ -48,8 +49,207 @@ GameCtrl.Preloader.prototype = {
 		//this.load.bitmapFont('caslon', 'assets/fonts/caslon.png', 'assets/fonts/caslon.xml');
 
 
+var botData={'frames': [
+    {
+    'filename': 'clown0000',
+    'frame': {'x':164,'y':5,'w':16,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':16,'h':24}
+    },
+    {
+    'filename': 'clown0001',
+    'frame': {'x':185,'y':5,'w':16,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':16,'h':24}
+    },
+    {
+    'filename': 'clown0002',
+    'frame': {'x':205,'y':5,'w':16,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':16,'h':24}
+    },
+    {
+    'filename': 'clownJump0003',
+    'frame': {'x':226,'y':5,'w':16,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':16,'h':24}
+    },
+    {
+    'filename': 'clownStand0000',
+    'frame': {'x':164,'y':58,'w':15,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':15,'h':24}
+    },
+    {
+    'filename': 'clownStandJump0000',
+    'frame': {'x':182,'y':58,'w':15,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':15,'h':24}
+    },
+    {
+    'filename': 'clownburn0000',
+    'frame': {'x':164,'y':32,'w':15,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':16,'h':24}
+    },
+    {
+    'filename': 'lion0002',
+    'frame': {'x':164,'y':87,'w':33,'h':16},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':33,'h':16},
+    'sourceSize': {'w':33,'h':16}
+    },
+    {
+    'filename': 'lion0001',
+    'frame': {'x':200,'y':87,'w':33,'h':16},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':33,'h':16},
+    'sourceSize': {'w':33,'h':16}
+    },
+    {
+    'filename': 'lion0000',
+    'frame': {'x':234,'y':87,'w':33,'h':16},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':33,'h':16},
+    'sourceSize': {'w':33,'h':16}
+    },
+    {
+    'filename': 'lionburn0000',
+    'frame': {'x':272,'y':87,'w':33,'h':16},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':33,'h':16},
+    'sourceSize': {'w':33,'h':16}
+    },
+    {
+    'filename': 'firepot0000',
+    'frame': {'x':221,'y':194,'w':24,'h':31},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':24,'h':31},
+    'sourceSize': {'w':24,'h':31}
+    },
+    {
+    'filename': 'firepot0001',
+    'frame': {'x':195,'y':194,'w':24,'h':31},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':24,'h':31},
+    'sourceSize': {'w':24,'h':31}
+    },
+    {
+    'filename': 'firecirclel0000',
+    'frame': {'x':136,'y':145,'w':12,'h':80},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':12,'h':80},
+    'sourceSize': {'w':12,'h':80}
+    
+    },
+    {
+    'filename': 'firecirclel0001',
+    'frame': {'x':165,'y':145,'w':12,'h':80},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':12,'h':80},
+    'sourceSize': {'w':12,'h':80}
+    },
+    {
+    'filename': 'firecircler0000',
+    'frame': {'x':148,'y':145,'w':12,'h':80},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':12,'h':80},
+    'sourceSize': {'w':12,'h':80}
+    },
+    {
+    'filename': 'firecircler0001',
+    'frame': {'x':177,'y':145,'w':12,'h':80},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':12,'h':80},
+    'sourceSize': {'w':12,'h':80}
+    },
+    {'filename': 'endLevel1',
+    'frame': {'x':129,'y':243,'w':37,'h':22},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':37,'h':22},
+    'sourceSize': {'w':37,'h':18}
+    },
+    {'filename': 'walkBalance0',
+    'frame': {'x':164,'y':5,'w':16,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':16,'h':24}
+    },
+    {'filename': 'walkBalance1',
+    'frame': {'x':185,'y':5,'w':15,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':15,'h':24},
+    'sourceSize': {'w':15,'h':24}
+    },
+    {'filename': 'walkBalance2',
+    'frame': {'x':205,'y':5,'w':16,'h':24},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':24},
+    'sourceSize': {'w':16,'h':24}
+    },
+    {'filename': 'jumpBalance',
+    'frame': {'x':226, 'y':7,'w':16,'h':22},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':22},
+    'sourceSize': {'w':16,'h':22}
+    },
+    {'filename': 'monkey0',
+    'frame': {'x':78, 'y':106,'w':16,'h':16},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':16},
+    'sourceSize': {'w':16,'h':16}
+    },
+    {'filename': 'monkey1',
+    'frame': {'x':98, 'y':106,'w':16,'h':16},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':16,'h':16},
+    'sourceSize': {'w':16,'h':16}
+    },
+    {'filename': 'monkey2',
+    'frame': {'x':118, 'y':106,'w':17,'h':16},
+    'rotated': false,
+    'trimmed': true,
+    'spriteSourceSize': {'x':0,'y':0,'w':17,'h':16},
+    'sourceSize': {'w':17,'h':16}
+    }    
+    ]};
 
-		//	+ lots of other required assets here
+
+    this.game.load.atlas('clown', 'assets/images/CircusCharlieSheet1.gif', null, botData);
+
+
+
 
 	},
 
